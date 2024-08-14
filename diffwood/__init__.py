272989a2aba86a4b4c9df3a5ee085142fecf870b
Kenji Tojo <kenjitojo@icloud.com>
2024-08-14 21:21:47 +0900

    add noise test

--- diffwood/__init__.py
+++ diffwood/__init__.py
@@ -1,2 +1,33 @@
+from typing import Union, Tuple, List, Any
 from . import cuda
 from .noise import *
+
+
+def gen_pix_samples(
+    resolution: Tuple[int, int],
+    spp_x = 1,
+    device = 'cpu',
+    random = False
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+
+    height, width = resolution
+    spp = spp_x ** 2
+
+    pos_x = torch.arange(width * spp_x, dtype=torch.float32, device=device)
+    pos_y = torch.arange(height * spp_x, dtype=torch.float32, device=device)
+    pos_x, pos_y = torch.meshgrid(pos_x, pos_y, indexing='xy')
+
+    if random:
+        pos_x = -1.0 + 2.0 * (pos_x + torch.rand_like(pos_x)) / width / spp_x
+        pos_y = -1.0 + 2.0 * (pos_y + torch.rand_like(pos_y)) / height / spp_x
+    else:
+        pos_x = -1.0 + 2.0 * (pos_x + 0.5) / width / spp_x
+        pos_y = -1.0 + 2.0 * (pos_y + 0.5) / height / spp_x
+
+    pos_x = pos_x.view(height, spp_x, width, spp_x).permute(0, 2, 1, 3)
+    pos_y = pos_y.view(height, spp_x, width, spp_x).permute(0, 2, 1, 3)
+    pos_x = pos_x.contiguous().view(height, width, spp, 1)
+    pos_y = pos_y.contiguous().view(height, width, spp, 1)
+
+    return pos_x, pos_y
+
--- test_diffwood.py
+++ test_diffwood.py
@@ -7,8 +7,26 @@ if __name__ == '__main__':
     print('cuda.is_available() =', dw.cuda.is_available())
 
 
-    p = 0.5 * torch.ones(100,3)
-    noise = dw.periodic_noise_3d(p)
+    height, width = 128, 128
+    x,y = dw.gen_pix_samples((height, width))
 
-    print(noise)
+    x = x.view(-1,1)
+    y = y.view(-1,1)
+    p = torch.cat((x, y, 0.5*torch.ones_like(x)), dim=1)
+
+    noise = dw.periodic_noise_3d(p).view(height, width, 3)
+
+
+
+    import os
+    import matplotlib.pyplot as plt
+
+    output_dir = './output'
+    os.makedirs(output_dir, exist_ok=True)
+
+    plt.clf()
+    plt.title('NoiseX')
+    plt.imshow(noise[..., 0])
+    plt.savefig(os.path.join(output_dir, './noise.png'))
+
 
