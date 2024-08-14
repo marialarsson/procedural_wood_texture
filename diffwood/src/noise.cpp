deddbd068fca23ebdad8c283252370e48400a188
Kenji Tojo <kenjitojo@icloud.com>
2024-08-14 20:57:33 +0900

    fix float precision

--- diffwood/src/noise.cpp
+++ diffwood/src/noise.cpp
@@ -14,13 +14,13 @@ void periodic_noise_3d(const float p[3], float out[3]) {
     noiseX += 0.5f * sin(4.0f * PI * px) * cos(4.0f * PI * py) * sin(4.0f * PI * pz);
     noiseX += 0.25f * sin(8.0f * PI * px) * cos(8.0f * PI * py) * sin(8.0f * PI * pz);
 
-    float noiseY = cos(2.0 * PI * px) * sin(2.0 * PI * py) * cos(2.0 * PI * pz);
-    noiseY += 0.5 * cos(4.0 * PI * px) * sin(4.0 * PI * py) * cos(4.0 * PI * pz);
-    noiseY += 0.25 * cos(8.0 * PI * px) * sin(8.0 * PI * py) * cos(8.0 * PI * pz);
+    float noiseY = cos(2.0f * PI * px) * sin(2.0f * PI * py) * cos(2.0f * PI * pz);
+    noiseY += 0.5f * cos(4.0f * PI * px) * sin(4.0f * PI * py) * cos(4.0f * PI * pz);
+    noiseY += 0.25f * cos(8.0f * PI * px) * sin(8.0f * PI * py) * cos(8.0f * PI * pz);
 
-    float noiseZ = sin(2.0 * PI * px + 1.0) * cos(2.0 * PI * py + 1.0) * sin(2.0 * PI * pz + 1.0);
-    noiseZ += 0.5 * sin(4.0 * PI * px + 1.0) * cos(4.0 * PI * py + 1.0) * sin(4.0 * PI * pz + 1.0);
-    noiseZ += 0.25 * sin(8.0 * PI * px + 1.0) * cos(8.0 * PI * py + 1.0) * sin(8.0 * PI * pz + 1.0);
+    float noiseZ = sin(2.0f * PI * px + 1.0f) * cos(2.0f * PI * py + 1.0f) * sin(2.0f * PI * pz + 1.0f);
+    noiseZ += 0.5f * sin(4.0f * PI * px + 1.0f) * cos(4.0f * PI * py + 1.0f) * sin(4.0f * PI * pz + 1.0f);
+    noiseZ += 0.25f * sin(8.0f * PI * px + 1.0f) * cos(8.0f * PI * py + 1.0f) * sin(8.0f * PI * pz + 1.0f);
 
     // Combine into vec3 and return the noise
     out[0] = noiseX;
